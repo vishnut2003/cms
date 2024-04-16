56d7006b129d8be876acbb836bf605c00c668ad9
vishnu <iam.vishnu.bhaskar@gmail.com>
2024-04-16 23:41:08 +0530

    Registration: user password encrypted using bcrypt module

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -1,8 +1,12 @@
 const db = require('../config/dbconnection')
 const collections = require('../config/collections')
+const bcrypr = require('bcrypt')
 
 module.exports = {
     registerUser: async (newUser) => {
+
+        newUser.password = await bcrypr.hash(newUser.password, 10)
+
         try{
             await db.getdb().collection(collections.USER_COLLECTIONS).insertOne(newUser);
         }catch(err){
